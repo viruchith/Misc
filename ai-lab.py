7e81279c36faa35c6d5c1046bd92763995002d69
Viruchith <58523045+viruchith@users.noreply.github.com>
2022-06-27 19:31:34 +0530

    Create ai-lab.py

---
+++ ai-lab.py
@@ -0,0 +1,416 @@
+# 1. A * Search
+def aStarAlgo(start_node, stop_node):
+         
+        open_set = set(start_node) 
+        closed_set = set()
+        g = {} #store distance from starting node
+        parents = {}# parents contains an adjacency map of all nodes
+ 
+        #ditance of starting node from itself is zero
+        g[start_node] = 0
+        #start_node is root node i.e it has no parent nodes
+        #so start_node is set to its own parent node
+        parents[start_node] = start_node
+         
+         
+        while len(open_set) > 0:
+            n = None
+ 
+            #node with lowest f() is found
+            for v in open_set:
+                if n == None or g[v] + heuristic(v) < g[n] + heuristic(n):
+                    n = v
+             
+                     
+            if n == stop_node or Graph_nodes[n] == None:
+                pass
+            else:
+                for (m, weight) in get_neighbors(n):
+                    #nodes 'm' not in first and last set are added to first
+                    #n is set its parent
+                    if m not in open_set and m not in closed_set:
+                        open_set.add(m)
+                        parents[m] = n
+                        g[m] = g[n] + weight
+                         
+     
+                    #for each node m,compare its distance from start i.e g(m) to the
+                    #from start through n node
+                    else:
+                        if g[m] > g[n] + weight:
+                            #update g(m)
+                            g[m] = g[n] + weight
+                            #change parent of m to n
+                            parents[m] = n
+                             
+                            #if m in closed set,remove and add to open
+                            if m in closed_set:
+                                closed_set.remove(m)
+                                open_set.add(m)
+ 
+            if n == None:
+                print('Path does not exist!')
+                return None
+ 
+            # if the current node is the stop_node
+            # then we begin reconstructin the path from it to the start_node
+            if n == stop_node:
+                path = []
+ 
+                while parents[n] != n:
+                    path.append(n)
+                    n = parents[n]
+ 
+                path.append(start_node)
+ 
+                path.reverse()
+ 
+                print('Path found: {}'.format(path))
+                return path
+ 
+ 
+            # remove n from the open_list, and add it to closed_list
+            # because all of his neighbors were inspected
+            open_set.remove(n)
+            closed_set.add(n)
+ 
+        print('Path does not exist!')
+        return None
+         
+#define fuction to return neighbor and its distance
+#from the passed node
+def get_neighbors(v):
+    if v in Graph_nodes:
+        return Graph_nodes[v]
+    else:
+        return None
+#for simplicity we ll consider heuristic distances given
+#and this function returns heuristic distance for all nodes
+def heuristic(n):
+        H_dist = {
+            'A': 11,
+            'B': 6,
+            'C': 99,
+            'D': 1,
+            'E': 7,
+            'G': 0,
+             
+        }
+ 
+        return H_dist[n]
+ 
+#Describe your graph here  
+Graph_nodes = {
+    'A': [('B', 2), ('E', 3)],
+    'B': [('C', 1),('G', 9)],
+    'C': None,
+    'E': [('D', 6)],
+    'D': [('G', 1)],
+     
+}
+aStarAlgo('A', 'G')
+
+#------------------------------------------------------------------------------------------------------------------
+
+# 2. Hill Climbing
+
+import random
+
+def randomSolution(tsp):
+    cities = list(range(len(tsp)))
+    solution = []
+
+    for i in range(len(tsp)):
+        randomCity = cities[random.randint(0, len(cities) - 1)]
+        solution.append(randomCity)
+        cities.remove(randomCity)
+    return solution
+
+def routeLength(tsp, solution):
+    routeLength = 0
+    for i in range(len(solution)):
+        routeLength += tsp[solution[i - 1]][solution[i]]
+    return routeLength
+
+def getNeighbours(solution):
+    neighbours = []
+    for i in range(len(solution)):
+        for j in range(i + 1, len(solution)):
+            neighbour = solution.copy()
+            neighbour[i] = solution[j]
+            neighbour[j] = solution[i]
+            neighbours.append(neighbour)
+    return neighbours
+
+def getBestNeighbour(tsp, neighbours):
+    bestRouteLength = routeLength(tsp, neighbours[0])
+    bestNeighbour = neighbours[0]
+    for neighbour in neighbours:
+        currentRouteLength = routeLength(tsp, neighbour)
+        if currentRouteLength < bestRouteLength:
+            bestRouteLength = currentRouteLength
+
+            bestNeighbour = neighbour
+    return bestNeighbour, bestRouteLength
+
+def hillClimbing(tsp):
+    currentSolution = randomSolution(tsp)
+    currentRouteLength = routeLength(tsp, currentSolution)
+    neighbours = getNeighbours(currentSolution)
+    bestNeighbour, bestNeighbourRouteLength = getBestNeighbour(tsp, neighbours)
+
+    while bestNeighbourRouteLength < currentRouteLength:
+        currentSolution = bestNeighbour
+        currentRouteLength = bestNeighbourRouteLength
+        neighbours = getNeighbours(currentSolution)
+        bestNeighbour, bestNeighbourRouteLength = getBestNeighbour(tsp, neighbours)
+
+    return currentSolution, currentRouteLength
+
+def main():
+    tsp = [
+        [0, 400, 500, 300],
+        [400, 0, 300, 500],
+        [500, 300, 0, 400],
+        [300, 500, 400, 0]
+
+    ]
+    print(hillClimbing(tsp))
+
+if __name__ == "__main__":
+    main()
+
+
+#------------------------------------------------------------------------------------------------------------------
+    
+    
+# 3. Multi Agent Swarm
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+	
+
+v0           = 0.5     
+eta          = 0.6     
+L            = 5       
+R            = 0.5      
+dt           = 0.1    
+Nt           = 200      
+N            = 300      
+plotRealTime = True
+
+
+np.random.seed(30)     
+
+
+x = np.random.rand(N,1)*L
+y = np.random.rand(N,1)*L
+
+
+theta = 2 * np.pi * np.random.rand(N,1)
+vx = v0 * np.cos(theta)
+vy = v0 * np.sin(theta)
+
+
+fig = plt.figure(figsize=(6,6), dpi=96)
+ax = plt.gca()
+
+
+for i in range(Nt):
+
+	
+	x += vx*dt
+	y += vy*dt
+	
+	
+	x = x % L
+	y = y % L
+	
+	
+	mean_theta = theta
+	for b in range(N):
+		neighbors = (x-x[b])**2+(y-y[b])**2 < R**2
+		sx = np.sum(np.cos(theta[neighbors]))
+		sy = np.sum(np.sin(theta[neighbors]))
+		mean_theta[b] = np.arctan2(sy, sx)
+		
+	
+	theta = mean_theta + eta*(np.random.rand(N,1)-0.5)
+	
+
+	vx = v0 * np.cos(theta)
+	vy = v0 * np.sin(theta)
+	
+
+	if plotRealTime or (i == Nt-1):
+		plt.cla()
+		plt.quiver(x,y,vx,vy,color='r')
+		ax.set(xlim=(0, L), ylim=(0, L))
+		ax.set_aspect('equal')	
+		plt.pause(0.001)
+
+
+
+plt.savefig('activematter.png',dpi=240)
+plt.show()
+
+#------------------------------------------------------------------------------------------------------------------
+
+# 4. N Queen 
+
+N = 4
+ld = [0] * 30
+rd = [0] * 30
+cl = [0] * 30
+ 
+def printSolution(board):
+    for i in range(N):
+        for j in range(N):
+            print(board[i][j], end = " ")
+        print()
+ 
+
+def solveNQUtil(board, col):
+    if (col >= N):
+        return True
+        
+    for i in range(N):
+        if ((ld[i - col + N - 1] != 1 and
+             rd[i + col] != 1) and cl[i] != 1):
+                  
+            board[i][col] = 1
+            ld[i - col + N - 1] = rd[i + col] = cl[i] = 1
+             
+            if (solveNQUtil(board, col + 1)):
+                return True
+            
+            board[i][col] = 0 
+            ld[i - col + N - 1] = rd[i + col] = cl[i] = 0
+    return False
+
+
+def solveNQ():
+    board = [[0, 0, 0, 0],
+             [0, 0, 0, 0],
+             [0, 0, 0, 0],
+             [0, 0, 0, 0]]
+    if (solveNQUtil(board, 0) == False):
+        printf("Solution does not exist")
+        return False
+    printSolution(board)
+    return True
+     
+solveNQ()
+
+#------------------------------------------------------------------------------------------------------------------
+
+# 5 Adversal search Tic Tac Toe
+
+#Implementation of Two Player Tic-Tac-Toe game in Python.
+
+
+
+theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
+            '4': ' ' , '5': ' ' , '6': ' ' ,
+            '1': ' ' , '2': ' ' , '3': ' ' }
+
+board_keys = []
+
+for key in theBoard:
+    board_keys.append(key)
+
+
+
+def printBoard(board):
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+
+
+def game():
+
+    turn = 'X'
+    count = 0
+
+
+    for i in range(10):
+        printBoard(theBoard)
+        print("It's your turn," + turn + ".Move to which place?")
+
+        move = input()        
+
+        if theBoard[move] == ' ':
+            theBoard[move] = turn
+            count += 1
+        else:
+            print("That place is already filled.\nMove to which place?")
+            continue
+
+        
+        if count >= 5:
+            if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ': # across the top
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")                
+                break
+            elif theBoard['4'] == theBoard['5'] == theBoard['6'] != ' ': # across the middle
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break
+            elif theBoard['1'] == theBoard['2'] == theBoard['3'] != ' ': # across the bottom
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break
+            elif theBoard['1'] == theBoard['4'] == theBoard['7'] != ' ': # down the left side
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break
+            elif theBoard['2'] == theBoard['5'] == theBoard['8'] != ' ': # down the middle
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break
+            elif theBoard['3'] == theBoard['6'] == theBoard['9'] != ' ': # down the right side
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break 
+            elif theBoard['7'] == theBoard['5'] == theBoard['3'] != ' ': # diagonal
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break
+            elif theBoard['1'] == theBoard['5'] == theBoard['9'] != ' ': # diagonal
+                printBoard(theBoard)
+                print("\nGame Over.\n")                
+                print(" **** " +turn + " won. ****")
+                break 
+
+       
+        if count == 9:
+            print("\nGame Over.\n")                
+            print("It's a Tie!!")
+
+       
+        if turn =='X':
+            turn = 'O'
+        else:
+            turn = 'X'        
+    
+   
+    restart = input("Do want to play Again?(y/n)")
+    if restart == "y" or restart == "Y":  
+        for key in board_keys:
+            theBoard[key] = " "
+
+        game()
+
+if __name__ == "__main__":
+    game()
+
