0c7b6d1c3bcdd18f27752075121effb64cf705a7
Viruchith <58523045+viruchith@users.noreply.github.com>
2022-06-27 20:14:00 +0530

    Update ai-lab.py

--- ai-lab.py
+++ ai-lab.py
@@ -506,4 +506,18 @@ print(response)
 response = chatbot.get_response('Who are you?')
 print(response)
 
-	
+# -----------------------------------------------------------------------------------------------------------
+
+# 8. Tower of Hanoi
+
+def TowerOfHanoi(n , source, destination, auxiliary):
+    if n==1:
+        print ("Move disk 1 from source",source,"to destination",destination)
+        return
+    TowerOfHanoi(n-1, source, auxiliary, destination)
+    print ("Move disk",n,"from source",source,"to destination",destination)
+    TowerOfHanoi(n-1, auxiliary, destination, source)
+         
+# Driver code
+n = 4
+TowerOfHanoi(n,'A','B','C')
