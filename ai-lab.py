6b7344b5c2c9d1ac9a803f6bef62063e0256a682
Viruchith <58523045+viruchith@users.noreply.github.com>
2022-06-27 19:43:59 +0530

    Update ai-lab.py

--- ai-lab.py
+++ ai-lab.py
@@ -461,3 +461,35 @@ if __name__ == "__main__":
 	s = 0
 	print(travellingSalesmanProblem(graph, s))
 
+# -----------------------------------------------------------------------------------------------------------
+
+# 7. Chatbot 
+
+# Import "chatbot" from
+# chatterbot package.
+from chatterbot import ChatBot
+
+# Inorder to train our bot, we have
+# to import a trainer package
+# "ChatterBotCorpusTrainer"
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+
+# Give a name to the chatbot “corona bot”
+# and assign a trainer component.
+chatbot=ChatBot('corona bot')
+
+# Create a new trainer for the chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Now let us train our bot with multiple corpus
+trainer.train("chatterbot.corpus.english.greetings",
+			"chatterbot.corpus.english.conversations" )
+
+response = chatbot.get_response('What is your Number')
+print(response)
+
+response = chatbot.get_response('Who are you?')
+print(response)
+
+	
